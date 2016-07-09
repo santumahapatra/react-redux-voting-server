5b43bf21454d13260e5bb118c3a50975816b4dea
Santu Mahapatra <santumahapatra@gmail.com>
2016-07-09 21:47:23 +0530

    Build the application logic

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -44,7 +44,7 @@ describe ('immutability', () => {
 
   describe('A Tree', () => {
     function addMovie(currentState, movie) {
-      return currentState.set('movies', currentState.get('movies').push(movie));
+      return currentState.update('movies', movies => movies.push(movie));
     }
 
     it('is immutable', () => {
